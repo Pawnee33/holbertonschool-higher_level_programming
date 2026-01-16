143a2feb8a4d01a768b47e7f55ee6c00527d7ca5
Pawnee <pawneedefize@hotmail.com>
2026-01-16 14:01:03 +0000

    fix: add variable execution at the line 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    args_numbers = len(sys.argv)
-    if args_numbers == 1:
-        print("{} argument:".format(args_numbers))
+    args_numbers = len(sys.argv) - 1
+    if args_numbers == 0:
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(args_numbers))
+        token = "argument" if args_numebers == 1 else "arguments"
+        print("{} {}: arguments:".format(args_numbers, token))
         for numbers, arg in enumerate(sys.argv[1:], start=1):
             print("{}: {}".format(numbers, arg))
