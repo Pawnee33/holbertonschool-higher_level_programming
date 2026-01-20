de69673941708973c66f7b0591178e2b9101de58
Pawnee <pawneedefize@hotmail.com>
2026-01-20 14:02:31 +0000

    test: function that adds 2 tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+    if len(tuple_a) >= 1:
+        vala0 = tuple_a[0]
+    else:
+        vala0 = 0
+    if len(tuple_a) >= 2:
+        vala1 = tuple_a[1]
+    else:
+        vala1 = 0
+    if len(tuple_b) >= 1:
+        valb0 = tuple_b[0]
+    else:
+        valb0 = 0
+    if len(tuple_b) >= 2:
+        valb1 = tuple_b[1]
+    else:
+        valb1 = 0
+    return vala0 + valb0, vala1 + valb1
