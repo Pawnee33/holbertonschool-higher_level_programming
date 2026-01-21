612970dbe4546d5286cdbdc9059ca1050950ffa6
Pawnee <pawneedefize@hotmail.com>
2026-01-21 16:55:24 +0000

    test: function that converts a Roman numeral to integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
     roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-     'C': 100, 'D': 500, 'M': 1000}
+    'C': 100, 'D': 500, 'M': 1000}
     total = 0
     for index in range(len(roman_string) - 1):
         actual_value = roman_dic[roman_string[index]]
