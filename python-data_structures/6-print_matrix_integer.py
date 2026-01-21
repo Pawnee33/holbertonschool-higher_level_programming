22876fad081bcd0bd5e281806e4797412ed966e9
Pawnee <pawneedefize@hotmail.com>
2026-01-21 08:08:06 +0000

    debug : exercise

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for index, integer in enumerate(row):
             if index != len(row) - 1:
-                print("{}".format(integer), end=" ")
+                print("{:d}".format(integer), end=" ")
             else:
-                print("{}".format(integer), end="")
+                print("{:d}".format(integer), end="")
         print()
