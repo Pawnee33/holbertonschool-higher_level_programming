e2785ecf8269607df144d0bcb3b8f6a8c1159153
Pawnee <pawneedefize@hotmail.com>
2026-01-21 09:22:14 +0000

    test: function that replaces all occurrences of an element by another in a new list

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for values in my_list:
+        if values == search:
+            values = replace
+            new_list.append(replace)
+        else:
+            new_list.append(values)
+    return new_list
