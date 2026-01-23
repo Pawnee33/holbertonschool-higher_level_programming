17e71136999332f6185a50940edcf26e886d95ae
Pawnee <pawneedefize@hotmail.com>
2026-01-23 13:39:31 +0000

    fix: line 36  over 89 lines1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
 This module contains a function to divide all elements of a matrix by a number.
-It checks for valid matrix structure, row sizes, element types,
-and div validity.
+It checks for valid matrix structure, row sizes, element types, and div validity.
 """
 
 
@@ -22,33 +21,35 @@ def matrix_divided(matrix, div):
         TypeError: if div is not a number
         ZeroDivisionError: if div is 0
     """
-    if not isinstance(matrix, list) or not all(
+    if not isinstance(matrix, list) or not matrix or not all(
         isinstance(row, list) for row in matrix
     ):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats"
         )
 
-    for row in matrix:
-        for element in row:
-            if not isinstance(element, (int, float)):
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) "
-                    "of integers/floats")
-
     row_length = len(matrix[0])
+    if row_length == 0:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
+
     for row in matrix:
         if len(row) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats"
+                )
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = []
-    for row in matrix:
-        new_row = [round(element / div, 2) for element in row]
-        new_matrix.append(new_row)
+    new_matrix = [
+        [round(element / div, 2) for element in row] for row in matrix
+    ]
 
     return new_matrix
