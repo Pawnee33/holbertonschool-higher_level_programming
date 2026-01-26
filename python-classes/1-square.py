ac106e5c69dd4b4e219c5887eb173fdc738f358a
Pawnee <pawneedefize@hotmail.com>
2026-01-26 14:43:27 +0000

    test: class Square that defines a squares

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#!/usr/binpython3
+"""
+This module is a class Square that define a square
+"""
+
+
+class Square:
+    """
+    Class Square define a square.
+    The class has a private attribute.
+
+    Attributes:
+        __size : Description of size
+
+    """
+    def __init__(self, size):
+        self.__size = size
