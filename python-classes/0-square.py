574bc60d08158101fc11706c3598286fd2d56986
Pawnee <pawneedefize@hotmail.com>
2026-01-26 13:56:20 +0000

    First commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,4 +5,9 @@ This module is an empty class Square that define a square
 
 
 class Square:
+    """
+    Square define an empty square.
+    And have not Attribute yet becauce we wante an empty class that define
+    square.
+    """
     pass
