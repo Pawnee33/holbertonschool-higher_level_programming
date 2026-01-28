bfe5fd5e1cac03fe51973d3346068f90b20ebdc1
Pawnee <pawneedefize@hotmail.com>
2026-01-28 09:52:09 +0000

    first commit

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module is an empty class Rectangle that define a square
+"""
+
+
+class Rectangle:
+    """
+    Rectangle define an empty rectangle.
+    And have not Attribute yet becauce we want an empty class that define
+    rectangle.
+    """
+    pass
