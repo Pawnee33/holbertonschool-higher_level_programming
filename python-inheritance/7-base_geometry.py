3f0c6c9ab987fe11e646945c3a13929fb4ea926e
Pawnee <pawneedefize@hotmail.com>
2026-02-03 16:28:18 +0000

    feat: add method to validating value

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -35,4 +35,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
