9dba30520930b04cb300de326208ec297abba30d
Pawnee <pawneedefize@hotmail.com>
2026-02-03 15:57:43 +0000

    add: exercise

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module is a class BaseGeometry raise an exception if
+the area is not implemented
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry define an empty an geometry.
+    And have not Attribute yet becauce we want an empty class that define
+    Geometry.
+    And raise an exception if the area is not implemented
+    """
+    def area(self):
+        """
+        area is a method that raise an exception if the area isn't
+        implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
