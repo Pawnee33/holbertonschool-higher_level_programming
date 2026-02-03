274159f230ced6f486295412245221da5c3b1894
Pawnee <pawneedefize@hotmail.com>
2026-02-03 13:48:05 +0000

    feat: creation of BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module is an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry define an empty an geometry.
+    And have not Attribute yet becauce we want an empty class that define
+    Geometry.
+    """
+    pass
