b3bd05e2c42a13315cb7a6d079768ae0d399dcaa
Pawnee <pawneedefize@hotmail.com>
2026-02-03 12:51:12 +0000

    feat: function that returns True if the object is an instance of a class or the inherited class specific

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def is_kind_of_class(obj, a_class):
+    """
+    is_kind_of_class is method that checks if obj come of
+    a specific class exactly or an inherited class.
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
