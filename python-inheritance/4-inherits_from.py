946c5b8ad2eee6ffcb290be99ba30a6b2ba92535
Pawnee <pawneedefize@hotmail.com>
2026-02-04 08:57:31 +0000

    debuging: add modele description

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,8 +10,5 @@ def inherits_from(obj, a_class):
     inherits_from checks if the object is an instance of class
     that inherited 'directly or indirectly' from the specified class
     """
-    
-    if type(obj) != a_class and issubclass(type(obj), a_class):
-        return True
-    else:
-        return False
+
+    return False if type(obj) is a_class else issubclass(type(obj), a_class)
