e2ed164d56fe513e73ec2ec04dd3c3fc84d9faf0
Pawnee <pawneedefize@hotmail.com>
2026-02-04 08:59:45 +0000

    debuging: add modele description

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -62,6 +62,6 @@ class Rectangle(BaseGeometry):
             ValueError: if width or height is <= 0.
         """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
