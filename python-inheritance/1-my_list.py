d6aab63c0a292f0c41099b789ae218ae2eaa4f4d
Pawnee <pawneedefize@hotmail.com>
2026-02-04 10:24:51 +0000

    debuging: add modele description

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""Module that defines a custom list class with
- a method to print it sorted.
- """
+"""Module that defines a custom list class with a method to print it sorted."""
 
 
 class MyList(list):
-    """
-    Mylist retrives the behaviour of class list.
-    """
+    """Class that extends list and adds a method to print it sorted."""
 
     def print_sorted(self):
-        """Prints the list in ascending sorted order without modifying it.
-        """
+        """Prints the list sorted in ascending order."""
         print(sorted(self))
