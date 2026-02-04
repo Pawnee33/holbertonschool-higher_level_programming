621fe4fdacf66bbe4b86611e552ae07422af3e0a
Pawnee <pawneedefize@hotmail.com>
2026-02-04 11:21:28 +0000

    debuging: add modele description

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,53 +2,7 @@
 """Module that defines BaseGeometry with area and integer validation."""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """
-    Rectangle is a subclass of BaseGeometry that represents a rectangle
-    shape with width and height as private attributes.
-    Width and height are validated as positive integers.
-    """
-    def __init__(self, width, height):
-        """
-        Initialize a Rectangle instance with width and height.
-
-        Args:
-            width : width of the rectangle, must be a positive integer.
-            height : height of the rectangle, must be a positive integer.
-
-        Raises:
-            TypeError: if width or height is not an integer.
-            ValueError: if width or height is <= 0.
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Calculate the area of the Rectangle.
-
-        Returns:
-            int: The area result of width * height.
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Return the string representation of the Rectangle in the format:
-        [Rectangle] <width>/<height>
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-
-"""
-A subclass module that inherits from BaseGeometry and representing
-square with validated size.
-"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
