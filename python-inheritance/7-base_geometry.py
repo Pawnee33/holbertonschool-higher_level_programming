e0c54b8c02a46ab3562ae39649ff62fe24bc7855
Pawnee <pawneedefize@hotmail.com>
2026-02-04 11:07:12 +0000

    debuging: add modele description

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,6 +4,7 @@
 
 class BaseGeometry:
     """Base class for geometry."""
+
     def area(self):
         """Raises an exception because area is not implemented."""
         raise Exception("area() is not implemented")
