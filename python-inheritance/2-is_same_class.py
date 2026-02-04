808c413fa5decdc7e14734ec876beabb12ad0eb3
Pawnee <pawneedefize@hotmail.com>
2026-02-04 08:52:03 +0000

    debuging: add modele description

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -10,5 +10,5 @@ def is_same_class(obj, a_class):
     Method checks if the object is an instance of
     the specific classand return True.
     """
-    
-    return type(obj) == a_class
+
+    return type(obj) is a_class
