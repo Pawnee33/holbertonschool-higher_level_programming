926e496854359c9fc38419cf791ccc3e77c65e7c
Pawnee <pawneedefize@hotmail.com>
2026-02-10 18:41:18 +0000

    feat: Module that defines a function to convert a JSON string into a Python object.

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a function to
+convert a JSON string into a Python object.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object represented by a JSON string."""
+    return json.loads(my_str)
