ecdb46860202406e55c8e449024749b3b307eb58
Pawnee <pawneedefize@hotmail.com>
2026-02-10 18:10:16 +0000

    feat: Module that writes a string to a text file (UTF8) and returns the number of characters written

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that writes a string to a text file (UTF8) and
+returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF-8 text file and
+    return the number of characters written.
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
