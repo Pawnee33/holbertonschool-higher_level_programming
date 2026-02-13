87e06fbb11bb973d94e5ddb29baf5011dbe3dc54
Pawnee <pawneedefize@hotmail.com>
2026-02-13 08:52:58 +0000

    first commit

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def serialize_and_save_to_file(data, filename):
+    with open(filename, 'w', encoding="utf-8") as f:
+        pass
+
+def load_and_deserialize(filename):
+    with open(filename, 'r', encoding="utf-8") as f:
+        pass
