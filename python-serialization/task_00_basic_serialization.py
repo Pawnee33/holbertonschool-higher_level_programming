f8a9dec400944c0f7a4c98bbdedfbb21b0695063
Pawnee <pawneedefize@hotmail.com>
2026-02-13 08:58:42 +0000

    first commit

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""Module serialization and deserialization of files"""
 import json
 
 
 def serialize_and_save_to_file(data, filename):
+    """Method serialization of filename to data"""
     with open(filename, 'w', encoding="utf-8") as f:
         json.dump(data, f)
 
 def load_and_deserialize(filename):
+    """Method deserialization of filename"""
     with open(filename, 'r', encoding="utf-8") as f:
         return json.load(f)
