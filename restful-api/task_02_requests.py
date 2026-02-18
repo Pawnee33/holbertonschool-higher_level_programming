eca48832818231976f67c5d5a23542299cdb2553
Pawnee <pawneedefize@hotmail.com>
2026-02-18 14:27:35 +0000

    feat: add task 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,5 +4,5 @@ import csv
 
 
 def fetch_and_print_posts():
-     response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
     print(f"Status Code: {response.status_code}")
