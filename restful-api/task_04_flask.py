bdd824c1b52742d307187d44d5bf27aa28973252
Pawnee <pawneedefize@hotmail.com>
2026-02-19 19:31:11 +0000

    feat: Simple Flask API with user management endpoints.

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,76 @@
+#!/usr/bin/pythpn3
+
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+users = {
+    "jane": {
+        "name": "Jane",
+        "age": 28,
+        "city": "Los Angeles"
+    },
+    "john": {
+        "username": "john",
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }
+}
+
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route('/data')
+def get_data():
+    return jsonify(list(users.keys()))
+
+
+@app.route('/status')
+def status():
+    return "OK"
+
+
+@app.route('/users/<username>')
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    if not request.is_json:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+    user_data = request.get_json()
+
+    username = user_data.get("username")
+
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 409
+
+    users[username] = {
+        "username": username,
+        "name": user_data.get("name"),
+        "age": user_data.get("age"),
+        "city": user_data.get("city")
+    }
+
+    return jsonify({
+        "message": "User added",
+        "user": users[username]
+    }), 201
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
