07d315b99a088f23beeeb8aac50ade2111be9a00
Pawnee <pawneedefize@hotmail.com>
2026-02-20 08:29:32 +0000

    fix:sheebang nomenclature error

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,23 +1,11 @@
-#!/usr/bin/pythpn3
+#!/usr/bin/python3
 
 from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
 
-users = {
-    "jane": {
-        "name": "Jane",
-        "age": 28,
-        "city": "Los Angeles"
-    },
-    "john": {
-        "username": "john",
-        "name": "John",
-        "age": 30,
-        "city": "New York"
-    }
-}
+users = {}
 
 
 @app.route('/')
