ead15cbb5715daf4f9a337888f2f9d86e453e233
Pawnee <pawneedefize@hotmail.com>
2026-03-03 17:53:26 +0000

    feat(no_link): add script that lists all records of the table second_table

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Script that lists all records of the table second_table
 -- Display both the score and the name
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
