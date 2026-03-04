af232c68533f0ee4bb7ebb88dd8fcf45b181286a
Pawnee <pawneedefize@hotmail.com>
2026-03-04 10:33:45 +0000

    feat(never_empty): add script that creates the table id_not_null on your MySQL server

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Script that creates table id_not_null on your MySQL server
+-- Ceates table id_not_null on your MySQL server
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
