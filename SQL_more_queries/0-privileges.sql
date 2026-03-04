e065604307b452c6ae4fbbca0ee6c44e2028acdb
Pawnee <pawneedefize@hotmail.com>
2026-03-04 09:12:28 +0000

    feat(privileges): add script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Script that lists all privileges of the MySQL users 'user_0d_1' on the server
+-- Display privileges of user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Script that lists all privileges of the MySQL users 'user_0d_2' on the server
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
