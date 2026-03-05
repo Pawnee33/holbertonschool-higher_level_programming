953876cb92bee8b10071a9181d3b7a621d584cc3
Pawnee <pawneedefize@hotmail.com>
2026-03-05 16:29:47 +0000

    feat(my_filter_states): add  script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module that connects to a MySQL database and lists states
+whose name matches a user provided argument, ordered by id.
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states "
+        "WHERE name = '{}' "
+        "ORDER BY id ASC".format(sys.argv[4])
+    )
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
