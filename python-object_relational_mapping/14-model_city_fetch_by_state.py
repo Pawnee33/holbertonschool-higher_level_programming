f1b8465f93658cae1e1a677ac407f966bfb3d8bc
Pawnee <pawneedefize@hotmail.com>
2026-03-06 14:27:45 +0000

    feat(join): add Module that connects to a MySQL database and displays all cities with their associated state name using SQLAlchemy.

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""Module that connects to a MySQL database and displays all cities
-with their associated state name using SQLAlchemy."""
+"""Displays all cities with their state name"""
 
 import sys
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
@@ -25,4 +27,4 @@ if __name__ == "__main__":
     )
 
     for state, city in cities:
-        print("{}: {} {}".format(state.name, city.id, city.name))
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
