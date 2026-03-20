d66ff0dd78115dcf8815cf94f029dd2803a04f2d
Pawnee <pawneedefize@hotmail.com>
2026-03-20 10:59:47 +0000

    feat: script that prints x times 'C is fun'

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ let Times = parseInt(process.argv[2]);
 if (isNaN(Times)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < Times; i++) {
+  for (let i = 0; i < Times; i--) {
     console.log('C is fun');
   }
 }
