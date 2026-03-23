0a8e151d92aecc78568c2a9d755291251e060839
Pawnee <pawneedefize@hotmail.com>
2026-03-23 10:40:59 +0000

    feat: Add  class

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const button = document.querySelector("#red_header");
+
+button.addEventListener("click", function() {
+    const header = document.querySelector("header");
+    header.classList.add("red");
+});
+
