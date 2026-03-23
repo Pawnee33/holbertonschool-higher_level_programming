53e825f83fef37deac3e3deb1a0bc3894c766869
Pawnee <pawneedefize@hotmail.com>
2026-03-23 10:20:38 +0000

    feat: Click and turn red

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const button = document.querySelector("#red_header");
+
+button.addEventListener("click", function() {
+    const header = document.querySelector("header");
+    header.style.color = "#FF0000";
+});
