3c174d894fc003558cab1c0b11854e59bdb8dd0b
Pawnee <pawneedefize@hotmail.com>
2026-03-24 14:20:47 +0000

    add exercise

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -5,19 +5,31 @@ import os
 def generate_invitations(template, attendees):
 
     if not isinstance(template, str):
-        raise TypeError("template must be an string")
+        print("Error: template must be a string")
+        return
     if not template:
-        raise ValueError("template is required")
-
+        print("Template is empty, no output files generated.")
+        return
     if not isinstance(attendees, list):
-        raise TypeError("attendees must be a list of dictionaries")
+        print("Error: attendees must be a list of dictionaries")
+        return
     if not attendees:
-        raise ValueError("attendees is required")
+        print("No data provided, no output files generated.")
+        return
 
     for index, attendee in enumerate(attendees, start= 1):
+        if not isinstance(attendee, dict):
+            print("Error: each attendee must be a dictionary")
+            return
+
         result = template
-        for key in ['name', 'event_title', 'event_data', 'event_location']:
+        for key in ['name', 'event_title', 'event_date', 'event_location']:
+            value = attendee.get(key)
             if value is None:
                 value = "N/A"
-            result = result.replace(f'{{{key}}}, str(value)')
+            result = result.replace(f'{{{key}}}', str(value))
 
+        filename = f"output_{index}.txt"
+
+        with open(filename, "w") as f:
+            f.write(result)
